d3d9f764bbf6fda1e7570b04bdf8f6f38488d048
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2023-11-09 11:24:56 +0100

    Added project

---
+++ Testing/2. TestDrivenDevelopment/cypress/e2e/movieSearch.cy.ts
@@ -0,0 +1,64 @@
+describe("movie search app", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173");
+  });
+
+  it("should have correct title", () => {
+    cy.get("h2").contains("Movie");
+    // cy.get("h2").should("contain.text", "Movie");
+  });
+
+  it("should contain the form", () => {
+    cy.get("form").should("exist");
+    cy.get("form input#searchText").should("exist");
+    cy.get("form button#search").contains("Search");
+  });
+
+  it("should show movies", () => {
+    cy.get("input#searchText").type("star");
+    cy.get("button#search").click();
+
+    cy.get("#search-result > div").should("have.length", 10);
+  });
+
+  it("should present a movie", () => {
+    cy.intercept("http://omdbapi.com/*", {
+      Search: [
+        {
+          Title: "The Matrix",
+          Poster: "http://someimage.png",
+        },
+      ],
+    });
+
+    cy.get("input#searchText").type("star");
+    cy.get("button#search").click();
+
+    cy.get("#search-result > div").should("have.length", 1);
+    cy.get("#search-result > div > h3").contains("The Matrix");
+    cy.get("#search-result > div > img").should(
+      "have.attr",
+      "src",
+      "http://someimage.png"
+    );
+  });
+
+  it("should show no results if no movies", () => {
+    cy.intercept("http://omdbapi.com/*", {
+      Search: [],
+    });
+
+    cy.get("input#searchText").type("harry");
+    cy.get("button#search").click();
+
+    cy.get("#search-result > p").contains("No movies found");
+  });
+
+  it("should not search if search text is less than 2 char", () => {
+    cy.get("button#search").click();
+
+    cy.get("#search-result > p").contains(
+      "Write at least three characters before searching"
+    );
+  });
+});
---
+++ Testing/2. TestDrivenDevelopment/src/ts/main.ts
@@ -0,0 +1,56 @@
+import axios from "axios";
+import "./../scss/style.scss";
+
+interface IOmdbResponse {
+  Search: IMovie[];
+}
+
+interface IMovie {
+  Title: string;
+  Poster: string;
+}
+
+document.getElementById("searchForm")?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const searchText = (document.getElementById("searchText") as HTMLInputElement)
+    .value;
+
+  const searchResult = document.getElementById("search-result");
+  if (searchResult) {
+    searchResult.innerHTML = "";
+  }
+
+  if (searchText.length <= 2) {
+    const validationError = document.createElement("p");
+    validationError.innerHTML =
+      "Write at least three characters before searching";
+
+    document.getElementById("search-result")?.appendChild(validationError);
+    return;
+  }
+
+  const response = await axios.get<IOmdbResponse>(
+    "http://omdbapi.com/?apikey=416ed51a&s=" + searchText
+  );
+
+  if (response.data.Search.length === 0) {
+    const noMoviesFound = document.createElement("p");
+    noMoviesFound.innerHTML = "No movies found";
+
+    document.getElementById("search-result")?.appendChild(noMoviesFound);
+  }
+
+  response.data.Search.forEach((movie) => {
+    const movieContainer = document.createElement("div");
+    const title = document.createElement("h3");
+    const image = document.createElement("img");
+
+    title.innerHTML = movie.Title;
+    image.src = movie.Poster;
+
+    movieContainer.appendChild(title);
+    movieContainer.appendChild(image);
+    searchResult?.appendChild(movieContainer);
+  });
+});
